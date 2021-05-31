0f878acd312926bc0e27adae48d32c9593dc8cd8
JowiAoun <83415433+JowiAoun@users.noreply.github.com>
2021-05-30 22:59:24 -0400

    Update travail2.js

--- travail2.js
+++ travail2.js
@@ -1,22 +1,30 @@
 var question = prompt("Quel forme voulez vous?")
 if (question == 1) {
+	document.write("Forme 1<br><br>")
   for (let i = 1; i<=10;) {
-    console.log("*".repeat(i))
+    document.write("*".repeat(i))
+	document.write("<br>")
     i++
   }
 } else if (question == 2) {
+	document.write("Forme 2<br><br>")
   for (let i = 10; i>=1;) {
-    console.log("*".repeat(i))
+    document.write("*".repeat(i))
+	document.write("<br>")
     i--
   }
 } else if (question == 3) {
-    for (let i = 10; i>=1;) {
-    console.log(" ".repeat(10-i) + "*".repeat(i))
+	document.write("Forme 3<br><br>")
+  for (let i = 10; i>=1;) {
+    document.write(" ".repeat(10-i) + "*".repeat(i))
+	document.write("<br>")
     i--
   }
 } else if (question == 4) {
-    for (let i = 1; i<=10;) {
-    console.log(" ".repeat(10-i) + "*".repeat(i))
+	document.write("Forme 4<br><br>")
+  for (let i = 1; i<=10;) {
+    document.write(" ".repeat(10-i) + "*".repeat(i))
+	document.write("<br>")
     i++
   }
 }
